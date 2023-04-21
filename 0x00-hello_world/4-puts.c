fa18949512358a45f1c4387986ad44f0b04e5ee5
naijatechgirle <kellyeluagu@gmail.com>
2023-04-20 22:04:20 -0700

    program that print using puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.header>
+/**
+ * main - A program that prints a line using the puts fxn
+ * Return: 0
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
