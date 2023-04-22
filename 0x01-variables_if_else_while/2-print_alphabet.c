61103e2000e5ec534291e0735cc3112c5e9d8954
naijatechgirle <kellyeluagu@gmail.com>
2023-04-22 02:00:21 -0700

    prints alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - entry point of functin
+ *
+ * program that prints function
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char alphabet = 'b';
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(alphabet);
+		letter++;
+	}
+	putchar('\n');
+
+	return (0);
+}
