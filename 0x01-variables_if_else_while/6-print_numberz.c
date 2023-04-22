51b131eef1ad729d4d8af6c5a84e013c53554496
naijatechgirle <kellyeluagu@gmail.com>
2023-04-22 03:54:30 -0700

    program that prints all single digit numbers  of base 10 starting from 0

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - entry of function
+ *
+ * program that prints all single digit numbers
+ * of base 10 starting from 0
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	putchar('0');
+	putchar('1');
+	putchar('2');
+	putchar('3');
+	putchar('4');
+	putchar('5');
+	putchar('6');
+	putchar('7');
+	putchar('8');
+	putchar('9');
+	putchar('\n');
+
+	return (0);
+}
