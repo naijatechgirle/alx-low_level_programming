f0d4f61e1d64b566bb5440cb797409466dbf307b
naijatechgirle <kellyeluagu@gmail.com>
2023-04-28 14:08:39 -0700

    modified header file

--- 0x04-more_functions_nested_loops/0-main.c
+++ 0x04-more_functions_nested_loops/0-main.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <ctype.h>
 #include <stdio.h>
 
 /**
@@ -6,13 +7,14 @@
  *
  * Return: Always 0.
  */
+
 int main(void)
 {
 	char c;
-	
+
 	c = 'A';
-	printf("%c: %d\n", c, _isupper(c));
+	printf("%c: %d\n", c, isupper(c));
 	c = 'a';
-	printf("%c: %d\n", c, _isupper(c));
+	printf("%c: %d\n", c, isupper(c));
 	return (0);
 }
