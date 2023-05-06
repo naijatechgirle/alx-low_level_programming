2c7dc41c0e63409270f358997b80f32458ada055
naijatechgirle <kellyeluagu@gmail.com>
2023-05-05 17:36:51 -0700

    modified prototype

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,7 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-char *_strncat(char *dest, char *src, int n);
+char *_strcat(char *dest, char *sr);
+char *_strncpy(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
