6c08ee137473a41e24d4b453aaecb0ed2488a1a3
naijatechgirle <kellyeluagu@gmail.com>
2023-05-05 19:29:12 -0700

    function that concatenates two strings.

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,14 +1,24 @@
 #include "main.h"
 
-/*
- * _strncat - function adds two  strings
- * @dest: final target
- * @src: starting point
- * @n: number of bytes read from src
- * Return: points to dest
+/**
+ * _strncat - concatenates two strings
+ *
+ * @dest: pointer to destination string
+ * @src: pointer to source string
+ * @n: maximum number of bytes to use from `src`
+ *
+ * Description: This function appends the `n`
+ * bytes of the `src` string to the
+ * end of the `dest` string, overwriting
+ * the null byte ('\0') at the  *end of
+ * `dest`, and then adds a terminating null byte.
+ * If `src` contains   *fewer than
+ * `n` bytes, the remaining bytes in `dest` are
+ * filled with null byte *s.
+ *
+ * Return: pointer to destination string `dest`
  */
-
-char *_strncat(char *dest, char *src, int n);
+char *_strncat(char *dest, char *src, int n)
 {
 	int i, j;
 
@@ -18,6 +28,7 @@ char *_strncat(char *dest, char *src, int n);
 	{
 		i++;
 	}
+
 	j = 0;
 
 	while (j < n && src[i] != '\0')
@@ -26,6 +37,7 @@ char *_strncat(char *dest, char *src, int n);
 		i++;
 		j++;
 	}
+
 	dest[i] = '\0';
 
 	return (dest);
