ce2ca72c02c47548775ab56f7deae6ad24d2c918
naijatechgirle <kellyeluagu@gmail.com>
2023-05-18 19:52:33 -0700

    0x0C. C - More malloc, free task

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * *malloc_checked - fxn that allocates memory using malloc
+ * @b: no of bytes to allocate
+ * Return: pointer to allocate memory
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+	ptr = malloc(b);
+
+	if (ptr == NULL)
+		exit(98);
+	return (ptr);
+}
