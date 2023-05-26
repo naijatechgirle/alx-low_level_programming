824b2f65bfc6e428f98ba5f44134dc484bb44b5e
naijatechgirle <kellyeluagu@gmail.com>
2023-05-25 22:01:42 -0700

    fixed new line

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -25,16 +25,16 @@ void print_all(const char * const format, ...)
 					printf("%s%c", sep, va_arg(list, int));
 					break;
 				case 'i':
-					printf("%s%d" , sep, va_arg(list, int));
+					printf("%s%d", sep, va_arg(list, int));
 					break;
 				case 'f':
-					printf("%s%f" , sep, va_arg(list, double));
+					printf("%s%f", sep, va_arg(list, double));
 					break;
 				case 's':
 					str = va_arg(list, char*);
 					if (!str)
 						str = "(nil)";
-					printf("%s%s" , sep, str);
+					printf("%s%s", sep, str);
 					break;
 				default:
 					i++;
@@ -44,6 +44,6 @@ void print_all(const char * const format, ...)
 		i++;
 		}
 	}
-	printf("\n ");
+	printf("\n");
 	va_end(list);
 }
