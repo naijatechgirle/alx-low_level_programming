c04c362470e0b325593025db68f47b766f8db2df
naijatechgirle <kellyeluagu@gmail.com>
2023-05-30 20:20:28 -0700

    debugging

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -11,17 +11,13 @@ int wildcmp(char *s1, char *s2)
 	{
 		if (*s2 != '\0' && *s2 == '*')
 		{
-			return (wildcmps(s1, s2 + 1));
+			return (wildcmp(s1, s2 + 1));
 		}
 		return (*s2 == '\0');
 	}
 	if (*s2 == '*')
 	{
-		return (wildcmps(s1 + s2) || wildcmps(s1, s2 + 1));
-	}
-	else if (*s1 == *s2)
-	{
-		return (wildcmps(s1 + 1, s2) || wildcmps(s1, s2 + 1));
+		return (wildcmp(s1 + s2) || wildcmp(s1, s2 + 1));
 	}
 	else if (*s1 == *s2)
 	{
