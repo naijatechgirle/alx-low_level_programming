ca0324a1910749691799b5923d83463907cf1b31
naijatechgirle <kellyeluagu@gmail.com>
2023-06-01 22:04:14 -0700

    debugged

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -10,14 +10,15 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new;
-	unsidned int len = 0;
+	
+	unsigned int len = 0;
 
 	while (str[len])
 		len++;
 
 	new = malloc(sizeof(list_t));
 
-	if(!new)
+	if (!new)
 		return (NULL);
 
 	new->str = strdup(str);
