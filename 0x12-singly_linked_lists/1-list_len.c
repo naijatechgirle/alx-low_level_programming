a0b44ccceb1b1d82eda87b1e6b375fb9e213f9a8
naijatechgirle <kellyeluagu@gmail.com>
2023-06-01 22:00:33 -0700

    solution for tasks on singly linked lists

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * list_len - return the num of elememts in a linked list
+ * @h: pointer to the list_t list
+ * Return: num of elements in h
+ */
+size_t list_len(const list_t *h)
+{
+	size_t n = 0;
+
+	while (h)
+	{
+		n++;
+		h = h-> next;
+	}
+	return (n);
+}
---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,29 @@
+#include <stdlib>
+#include <string.h>
+#include "lists.h"
+/**
+ * add_node - add new node
+ * @head: double pointer to list_t list
+ * @str: new string to add in the node
+ * Return: address other beautiful
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new;
+	unsidned int len = 0;
+
+	while (str[len])
+		len++;
+
+	new = malloc(sizeof(list_t));
+
+	if(!new)
+		return (NULL);
+
+	new->str = strdup(str);
+	new->len = len;
+	new->next = (*head);
+	(*head) = new;
+
+	return (*head);
+}
