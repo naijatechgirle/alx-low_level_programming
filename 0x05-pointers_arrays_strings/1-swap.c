e688a4b6411d11b2a45d237e9a7e380877aab694
naijatechgirle <kellyeluagu@gmail.com>
2023-06-04 12:26:19 -0700

    fxn taht swaps two integers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#inlcude "main.h"
+/**
+ * swap_int - fxn to swap two integers
+ * @a: first input
+ * @b: second input
+ * Return: void
+ */
+void swap_int(int *a, int *b)
+{
+	int p = *a;
+	*a = *b;
+	*b = p;
+}
