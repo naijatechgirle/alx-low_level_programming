f58aead67c45cde1f138a89c217dd1331419e029
naijatechgirle <kellyeluagu@gmail.com>
2023-06-04 13:09:15 -0700

    unction that prints a string, followed by a new line, to stdout.

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _puts - prints string
+ * @str: string to be printed
+ * Return: void
+ */
+void _puts(char *str)
+{
+	int count = 0;
+
+	while (count >= 0)
+	{
+		if (str[count] == '\0';
+			_putchar('\n');
+
+		_putchar(str[count]);
+		count++;
+	}
+}
