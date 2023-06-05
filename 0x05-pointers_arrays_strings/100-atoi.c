6c3c9768cdb68bc3db0437872255bcb85458b48a
naijatechgirle <kellyeluagu@gmail.com>
2023-06-04 21:09:22 -0700

    refactor code

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -23,22 +23,22 @@ int _atoi(char *s)
 		if (s[i] == '-')
 			++d;
 
-		if (S[i] >= '0' && s[i] <= '9')
+		if (s[i] >= '0' && s[i] <= '9')
 		{
 			digit = s[i] - '0';
-
 			if (d % 2)
 				digit = -digit;
 			n = n * 10 + digit;
 			f = 1;
-
 			if (s[i + 1] < '0' || s[i + 1] > '9')
 				break;
 			f = 0;
 		}
 		i++;
 	}
+
 	if (f == 0)
 		return (0);
+
 	return (n);
 }
