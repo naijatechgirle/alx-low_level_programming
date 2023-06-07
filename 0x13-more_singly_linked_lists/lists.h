5037ce5b16c85025e3b278da93c8aae8aa4719ac
naijatechgirle <kellyeluagu@gmail.com>
2023-06-06 17:26:42 -0700

    fixed typos

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -5,16 +5,17 @@
 #include <stdlib.h>
 
 /**
- * struct listint_S - singly linked list
+ * struct listint_s - singly linked list
  * @n: integer
  * @next: points to the next node
  *
- * Description: singly linked list nide structure
+ * Description: singly linked list node structure
+ *
  */
 typedef struct listint_s
 {
 	int n;
-	struct listint_s*next
+	struct listint_s *next
 } listint_t;
 
 size_t print_listint(const listint_t *h);
